0f96fbced02069e9522149f8310d0fa4a6aadfa5
Neil Elder <rneilelder@gmail.com>
2017-03-13 14:32:33 -0400

    Reorganized the repository structure

---
+++ json/balanceRequest.js
@@ -0,0 +1,24 @@
+{
+  "session": {
+    "sessionId": "SessionId.8d616913-e988-4156-93ad-b33b4872190a",
+    "application": {
+      "applicationId": "amzn1.ask.skill.1a02237d-01fd-422d-a648-0c172009e1b9"
+    },
+    "attributes": {},
+    "user": {
+      "userId": "amzn1.ask.account.AGTCKL5BBO2HFQCQVVPLUVCQY2QPOSP4WQIADN2L33JOZLJ3244J27R2ACHI723GJIXW6BY37UFP6KJ2CABU3N57UTS2MWL7TRD7K2BN37V4HL6GMD57ZIUQ4LZLUTQTFL7BAG5QFDJAGITSONDGFIFW5U42LJKUXQDQSRB52MMZFKAZSI3T4DHQMR4JPYJ3DQJ5OW6SGFS7TTY"
+    },
+    "new": true
+  },
+  "request": {
+    "type": "IntentRequest",
+    "requestId": "EdwRequestId.46f820e6-b6d9-46c6-9dd3-83b186833323",
+    "locale": "en-US",
+    "timestamp": "2017-03-13T18:22:01Z",
+    "intent": {
+      "name": "BalanceIntent",
+      "slots": {}
+    }
+  },
+  "version": "1.0"
+}
