ae51e5915a50a4a36dafd4aaa753ae7e06bb0894
Neil Elder <rneilelder@gmail.com>
2017-03-24 11:32:46 -0400

    Refactored the the payment intent to match project design
    Tested and worked as designed

--- src/index.js
+++ src/index.js
@@ -7,48 +7,57 @@ var PACE = require("./api/pace");
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = APP_ID;
-	alexa.registerHandlers(balanceHandlers, paymentHandlers, confirmationHandlers);
+    alexa.registerHandlers(balanceHandlers, paymentHandlers, confirmationHandlers);
     alexa.execute();
 };
 
 var callPace = function(options, callback, context){
-			 const req = https.request(options, (res) => {
-		 	let body = '';
-		 	console.log('Status:', res.statusCode);
-			console.log('Headers:', JSON.stringify(res.headers));
-		 	res.setEncoding('utf8');
-		 	res.on('data', (chunk) => body += chunk);
-		 	res.on('end', () => {
-		 		console.log('Successfully processed HTTPS response');
-		 		// If we know it's JSON, parse it
-		 		if (res.headers['content-type'] === 'application/json') {
-		 			callback.call(context, body); 
-		 		}
-		 	});
-		 });
-		 req.end();	 
+             const req = https.request(options, (res) => {
+            let body = '';
+            console.log('Status:', res.statusCode);
+            console.log('Headers:', JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => body += chunk);
+            res.on('end', () => {
+                console.log('Successfully processed HTTPS response');
+                // If we know it's JSON, parse it
+                if (res.headers['content-type'] === 'application/json') {
+                    callback.call(context, body); 
+                }
+            });
+         });
+         if (options.method === "POST"){
+            var theAmount = parseInt(context.event.request.intent.slots.payment.value) * 100;
+            console.log('paymentAmount.value: ', theAmount);
+            var obj = {
+                "amount": theAmount
+            };
+            var postJson = JSON.stringify(obj);
+            console.log('Post body: ', postJson);
+            req.write(postJson);
+         }
+         req.end();  
 };
 
 var speechResponse = function(body){
-	body = JSON.parse(body);
-	console.log('Body: ', body);
-	var speechOutput;
+    body = JSON.parse(body);
+    console.log('Body: ', body);
+    var speechOutput;
 
-	if(this.event.request.intent.name === 'BalanceIntent'){
-		speechOutput = "Your balance is $" + body.balance/100;		
+    if(this.event.request.intent.name === 'BalanceIntent'){
+        speechOutput = "Your balance is $" + body.balance/100;      
         this.event.session.attributes.fullamount =  body.balance/100;
-		speechOutput += " with your next payment of $" + body.amount_due/100;
-		speechOutput += " due on " + body.next_payment_date;
-		this.emit(":ask", speechOutput, "What would you like to do now? Say make a payment, or, i'm done");
-		//Alexa: How much would you like topay
-	}
-	// if(this.event.request.intent.name === 'PaymentIntent'){
-		// speechOutput = "Your credit card ending in " + body.card_last4;		
-		// speechOutput += " has been charged $" + body.amount/100;
-		// speechOutput += " your new balance is $" + body.new_balance/100;
-		// speechOutput += " due on " + body.next_payment_date;
-		// this.emit(":tell",speechOutput);
-	// }
+        speechOutput += " with your next payment of $" + body.amount_due/100;
+        speechOutput += " due on " + body.next_payment_date;
+        this.emit(":ask", speechOutput, "What would you like to do now? Say make a payment, or, i'm done");
+    }
+    if (this.event.request.intent.name === 'PaymentIntent') {
+        speechOutput = 'Your credit card ending in <say-as interpret-as="digits">' + body.card_last4 + '</say-as>';     
+        speechOutput += " has been charged $" + body.amount/100;
+        speechOutput += " your new balance is $" + body.new_balance/100;
+        speechOutput += " due on " + body.next_payment_date;
+        this.emit(":ask", speechOutput, "What would you like to do now, say send a confirmation, or, i'm done");
+    }
     if (this.event.request.intent.name === 'ConfirmationIntent') {
         speechOutput = "Confirmation email was sent to ";
         speechOutput += body.recipient;
@@ -59,12 +68,12 @@ var speechResponse = function(body){
 
 var balanceHandlers = {
     "BalanceIntent": function(){
-		if (this.event.session.user.accessToken == undefined) {
+        if (this.event.session.user.accessToken == undefined) {
             this.emit(':tellWithLinkAccountCard', 'to start using this skill, please use the companion app to authenticate on Amazon');
             return;
         }
-		callPace(PACE.getBalance(this.event.session.user.accessToken), speechResponse, this);
-	}
+        callPace(PACE.getBalance(this.event.session.user.accessToken), speechResponse, this);
+    }
 };
 
 var paymentHandlers = {
@@ -77,37 +86,7 @@ var paymentHandlers = {
             this.emit(':ask', 'Please specify the amount you want to pay', 'for example one dollar or pay in full');
             return;
         }
-        const req = https.request(PACE.postPayment(this.event.session.user.accessToken), (res) => {
-            let body = '';
-            console.log('Status:', res.statusCode);
-            console.log('Headers:', JSON.stringify(res.headers));
-            res.setEncoding('utf8');
-            res.on('data', (chunk) => body += chunk);
-            res.on('end', () => {
-                console.log('Successfully processed HTTPS response');
-                // If we know it's JSON, parse it
-                if (res.headers['content-type'] === 'application/json') {
-                    body = JSON.parse(body);
-                    console.log('Body: ', body);
-                    var speechOutput = 'Your credit card ending in <say-as interpret-as="digits">' + body.card_last4 + '</say-as>';     
-                    speechOutput += " has been charged $" + body.amount/100;
-                    speechOutput += " your new balance is $" + body.new_balance/100;
-                    speechOutput += " due on " + body.next_payment_date;
-                    this.emit(":ask", speechOutput, "What would you like to do now, say send a confirmation, or, i'm done");
-                }
-            });
-        });
-        var theAmount = parseInt(this.event.request.intent.slots.payment.value) * 100;
-        console.log('paymentAmount.value: ', theAmount);
-        var obj = {
-            "amount": theAmount
-        };
-        var postJson = JSON.stringify(obj);
-        console.log('Post body: ', postJson);
-        req.write(postJson);
-        req.end();
-		//theAmount = theAmount / 100;
-		//this.emit(":ask", "Your payment of $" +  theAmount + " was successful, what would you like to do now", "Say send a confirmation, or, i'm done");
+        callPace(PACE.postPayment(this.event.session.user.accessToken), speechResponse, this);
     },
 
     "PayInFullIntent": function(){
@@ -115,26 +94,6 @@ var paymentHandlers = {
             this.emit(':tellWithLinkAccountCard', 'to start using this skill, please use the companion app to authenticate on Amazon');
             return;
         }
-        // const req = https.request(PACE.postPayment(this.event.session.user.accessToken), (res) => {
-        //     let body = '';
-        //     console.log('Status:', res.statusCode);
-        //     console.log('Headers:', JSON.stringify(res.headers));
-        //     res.setEncoding('utf8');
-        //     res.on('data', (chunk) => body += chunk);
-        //     res.on('end', () => {
-        //         console.log('Successfully processed HTTPS response');
-        //         // If we know it's JSON, parse it
-        //         if (res.headers['content-type'] === 'application/json') {
-        //             body = JSON.parse(body);
-        //             console.log('Body: ', body);
-        //             var speechOutput = 'Your credit card ending in <say-as interpret-as="digits">' + body.card_last4 + '</say-as>';     
-        //             speechOutput += " has been charged $" + body.amount/100;
-        //             speechOutput += " your new balance is $" + body.new_balance/100;
-        //             speechOutput += " due on " + body.next_payment_date;
-        //             this.emit(":tell",speechOutput);
-        //         }
-        //     });
-        // });
         var theAmount = this.event.session.attributes.fullamount;
         this.emit(':tell', 'Amount to be paid ' + theAmount);
         console.log('attributes.fullamount: ', theAmount);
@@ -143,28 +102,26 @@ var paymentHandlers = {
         };
         var postJson = JSON.stringify(obj);
         console.log('Post body: ', postJson);
-        // req.write(postJson);
-        // req.end();
-		this.emit(":ask", "Your payment of $" +  theAmount + " was successful, what would you like to do now", "Say send a confirmation, or, i'm done");
+        this.emit(":ask", "Your payment of $" +  theAmount + " was successful, what would you like to do now", "Say send a confirmation, or, i'm done");
     }
 };
 
 var confirmationHandlers = {
     "ConfirmationIntent": function () {
-		if (this.event.session.user.accessToken == undefined) {
+        if (this.event.session.user.accessToken == undefined) {
             this.emit(':tellWithLinkAccountCard', 'to start using this skill, please use the companion app to authenticate on Amazon');
             return;
-        }		
+        }       
         console.log('In ConfirmationIntent');
         callPace(PACE.sendConfirmation(this.event.session.user.accessToken), speechResponse, this);
     },
     "CompletionIntent": function () {
-		if (this.event.session.user.accessToken == undefined) {
+        if (this.event.session.user.accessToken == undefined) {
             this.emit(':tellWithLinkAccountCard', 'to start using this skill, please use the companion app to authenticate on Amazon');
             return;
-        }		
+        }       
         console.log('In CompletionIntent');
-		this.emit(":tell", "Thank you for insuring with Homesite, goodbye");
+        this.emit(":tell", "Thank you for insuring with Homesite, goodbye");
     }
 };
 
